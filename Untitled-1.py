141dd8cdf022d9823eeec38a672fdfeb857c0041
KaranS3180 <22103180@mail.jiit.ac.in>
2024-08-29 16:27:49 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,53 @@
+
+class students:
+    def __init__(self):
+        self.student= []
+
+    def mark_present(self,s):
+        if s not in self.student:
+            self.student.append(s)
+            print("add successful")
+
+        else:
+            print("not added")
+
+    def remove_student(self,s):
+        if s in self.student:
+            self.student.remove(s)
+            print("removed")
+
+        else:
+            print("not removed as not present")
+
+    def is_present(self,s)->bool:
+        if s in self.student:
+            print("the entered student is present ")
+            return True
+        else:
+            print("the enetered student is nor present")
+            return False
+        
+    def display_attendence(self):
+        if self.student:
+            for i in self.student:
+                print(i)
+
+        else:
+            print("empty list")
+
+s=students()
+s.mark_present("karan")
+s.mark_present("rahul")
+s.mark_present("james")
+s.mark_present("frank")
+s.mark_present("oppoi")
+s.display_attendence()
+
+s.remove_student("karan")
+s.display_attendence()
+
+s.is_present("rahul")
+
+
+
+        
